cdae36ea97d48b6842510cda662c4840b618559c
gentleseal <gentleseal@users.noreply.github.com>
2017-09-13 19:45:52 +0100

    Remove unused files

--- ara/assets/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#ara
-sed -i -e "s/localhost/$mysql_server/" -e "s/usrname/$mysql_login/" -e "s/passwd2/$mysql_passwd/" /ara/src/config/config.php
-sed -i "s/expass2/$admin_passwd/" /ara/src/config/users/admin.php
